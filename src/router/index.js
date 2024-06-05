e3effb28d0f48491110b2fd0efc7044818179e01
Jomerstein <jomerstein@gmail.com>
2024-06-05 20:36:38 -0300

    Segundo push de clase

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
     component: Login
   },
   {
-    path: '/carDetail',
+    path: '/carDetail/:id',
     name: 'CarDetail',
     component: CarDetail
   },
--- src/views/CarDetail.vue
+++ src/views/CarDetail.vue
@@ -1,14 +1,25 @@
 <template lang="">
     <h1>Información del Auto</h1>
     <br></br>
+    <button v-if=isAdmin >Eliminar auto</button> 
+    <!-- esto no va a funcionar hasta que hagamos el sistema de autenticación -->
+    <button v-if=isAdmin >Modificar</button>
+    <button>Alquilar</button>
     <Car />
 </template>
 
 <script>
 import Car from '../components/Car.vue'
+import { useAuthStore } from '@/stores/userStore';
 export default {
     components: {
         Car
+    },
+    methods:{
+        isAdmin(){
+            const authStore = useAuthStore()
+            return authStore.isAdmin()
+        }
     }
 }
 </script>
